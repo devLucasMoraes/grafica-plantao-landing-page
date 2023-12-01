6a3c3792c63aee99d2b2dafcdc7f1f671c2168af
devLucasMoraes <devlucasmoraes@gmail.com>
2023-12-01 17:50:24 -0300

    feat:

---
+++ src/lib/Header.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Navbar from "./Navbar.svelte";
+</script>
+
+<nav class="shadow bg-sky-500">
+  <div
+    class="container flex items-center justify-center p-6 mx-autotext-gray-200"
+  >
+    <Navbar />
+  </div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,11 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script>
+  import Footer from "$lib/Footer.svelte";
+  import Header from "$lib/Header.svelte";
+  import "../app.pcss";
+</script>
+
+<Header />
+
+<slot />
+
+<Footer />
